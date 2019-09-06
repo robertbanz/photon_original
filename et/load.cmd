902b13116e4e11e0c62186dc12af58f599855a45
Robert Banz <rob@nofocus.org>
2019-09-05 17:30:39 -0700

    etsrc

---
+++ et/load.cmd
@@ -0,0 +1,8 @@
+CODE 0
+DATA 2000H
+STACK 2800H
+LOAD COLD,MENUS,SCN,EDITNM,LASERID,GUN,KEYSTART,TRNSFR,SERIO,ANCSUBS
+LOAD TASKHND,TERMBIOS,MENUSUBS
+END
+ 
+
